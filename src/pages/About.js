e9d433f25b9c5f79d155299d4cded6a67fa5eb58
chess2022 <lakeshore.hnh@gmail.com>
2022-05-21 13:18:22 -0700

    Frontend complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,28 @@
+import { useState, useEffect } from "react";
+
+
 export default function About(props) {
-  return <h1>About</h1>;
+  // Create state to hold about data
+  const [about, setAbout] = useState(null);
+
+  // Create api call function
+  const getAboutData = async () => {
+    const response = await fetch(props.URL + "about");
+    const data = await response.json();
+    setAbout(data);
+  };
+
+  // make an initial call for the data inside a useEffect
+  useEffect(() => getAboutData(), []);
+
+  const loaded = () => (
+    <div>
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bio}</p>
+    </div>
+  );
+
+  // if data arrives, return the result, if not an <h1> that says loading
+  return about ? loaded() : <h1>Loading...</h1>;
 }
